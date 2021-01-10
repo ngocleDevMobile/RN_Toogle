114f41e437732fea676190cd9828e42c9cd187df
lexuanngoc <ngoc1997307@gmail.com>
2021-01-10 15:34:21 +0700

    Fix useNativeDriver

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, LogBox} from 'react-native';
 import RN_toogle from './src/components/Toogle';
 
 const App = () => {
   const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+}, [])
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text style={{color: '#e27802', fontSize: 25, flexWrap: 'wrap'}}>Custom Toogle Switch(Android,Ios)</Text>
